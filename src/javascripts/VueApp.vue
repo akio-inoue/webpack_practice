05a63cd79c6d22dcd2c92243f78c75f91340137c
akio-inoue <100137555+akio-inoue@users.noreply.github.com>
2025-03-23 01:36:12 +0900

    ES6, React, Vue, etc...

---
+++ src/javascripts/VueApp.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data: () => {
+    console.log("akiotyan");
+    return {
+      message: "akiotyan2",
+    };
+  },
+};
+</script>
+
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<style scoped>
+p {
+  color: green;
+}
+</style>
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,12 @@
+import "./reactApp.jsx";
 import my from "./my.js";
 import "../stylesheets/main.scss";
+
+import { createApp } from "vue";
+import VueApp from "./VueApp.vue";
+
+const app = createApp(VueApp);
+app.mount("#vue-root");
 
 console.log("This is index.js");
 my();
