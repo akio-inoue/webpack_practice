db4c21c089933850836fdbd4a40504e7f8b38d41
akio-inoue <100137555+akio-inoue@users.noreply.github.com>
2025-04-12 10:06:29 +0900

    Install Typescript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,8 +5,11 @@ import "../stylesheets/main.scss";
 import { createApp } from "vue";
 import VueApp from "./VueApp.vue";
 
+import add from "./add.ts";
+
 const app = createApp(VueApp);
 app.mount("#vue-root");
 
+console.log(add(3, 9));
 console.log("This is index.js");
 my();
